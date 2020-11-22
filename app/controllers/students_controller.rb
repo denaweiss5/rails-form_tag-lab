82324623331119a66ff95fa46839d9b3e4a7aaae
https://learn.co/DenaNeuman <denaweiss5@gmail.com>
2020-11-21 21:19:41 -0600

    commit tree 9f17af3a1b6c6957d0f4a4e9135541e1b841eb33

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,18 +5,16 @@ class StudentsController < ApplicationController
   end
 
   def new
-    render "new"
   end
 
   def create
     @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
     redirect_to students_path(@student)
+  
   end
 
   def show
     @student = Student.find(params[:id])
   end
-
-
 
 end
